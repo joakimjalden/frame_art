a06453e653635c32e23ccec16d3409b295e6bc90
TheFab21 <121039071+TheFab21@users.noreply.github.com>
2025-10-10 18:12:16 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
-"""Samsung The Frame Art Switch."""
+from __future__ import annotations
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SWITCH])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_unload_platforms(entry, [Platform.SWITCH])
---
+++ const.py
@@ -0,0 +1,16 @@
+DOMAIN = "frame_art"
+CONF_MEDIA_PLAYER = "media_player"
+CONF_APP_ID = "app_id"
+CONF_SOURCE_OFF = "source_off"
+CONF_SELECT_DELAY = "select_delay"
+CONF_RETRIES = "retries"
+CONF_RETRY_SLEEP = "retry_sleep"
+CONF_RESOURCE = "resource"
+CONF_NAME = "name"
+
+DEFAULT_SELECT_DELAY = 0.6
+DEFAULT_RETRIES = 6
+DEFAULT_RETRY_SLEEP = 0.35
+DEFAULT_TIMEOUT = 5.0
+
+PLATFORMS = ["switch"]
